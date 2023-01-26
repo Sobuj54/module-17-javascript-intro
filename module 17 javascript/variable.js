5aee2bcd0a05ff8e9064d69deba5eca924476143
sobuj <sobujahmed124@gmail.com>
2023-01-26 19:55:22 +0600

    js files

---
+++ module 17 javascript/variable.js
@@ -0,0 +1,7 @@
+// vary +  able = variable
+// variable declaration : var variable-name = value;
+// here var is a keyword for variable in javascript.
+var price = 100
+var weight = 30
+var fair = 28
+var temparature = 22
