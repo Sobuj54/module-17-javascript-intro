e7f56b8101e6b7beab07b04cfc05ca19d5572a7e
sobuj <sobujahmed124@gmail.com>
2023-01-26 23:58:25 +0600

    short hand forms

---
+++ short_hand.js
@@ -0,0 +1,36 @@
+// declaring variable
+var tomatoPrice = 25
+
+// update value of a varibe
+tomatoPrice = 50
+console.log(tomatoPrice)
+
+// add value to a variable
+var x = 25
+var y = 63
+
+// adding value to x
+x = x + 10
+console.log(x)
+// shorthand of adding value
+x += 10
+console.log(x)
+// shorthand for subtraction
+x -= 20
+console.log(x)
+// shorthand for multiplication
+y *= 2
+// this means y = y * 2
+console.log(y)
+// short hand for division
+y /= 2
+console.log(y)
+
+// adding one value
+var z = 3
+// this ++ adding 1 to the value
+z++
+console.log(z)
+// subtracting 1
+z--
+console.log(z)
