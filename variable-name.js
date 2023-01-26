8672b9407b58fb6eb194642055698aabd1e64c80
sobuj <sobujahmed124@gmail.com>
2023-01-26 20:37:01 +0600

    variable name rules

---
+++ variable-name.js
@@ -0,0 +1,18 @@
+// variable name declaration rules
+// 1. variable name can not be a keyword.
+// 2. variable name can not contain any white space.
+
+// 3. variable name can not have quotaion.
+// 4. variable name can not start with any number but can contain number inside.
+
+//5. variable names are case sensitive.
+
+// 6. how to use long names
+// long names can not contain - this
+
+// snake case
+var your_name = 'sobuj'
+// camel case
+var yourName = 'sobuj'
+// pascal case
+var YourName = 'sobuj'
