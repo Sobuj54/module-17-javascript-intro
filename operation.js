d6ad3256f211f8906d38ac68a4d82cd4e8379e3c
sobuj <sobujahmed124@gmail.com>
2023-01-26 21:50:25 +0600

    oprations

---
+++ operation.js
@@ -0,0 +1,19 @@
+// declaring variables
+var eggPrice = 10
+var ricePrice = 75
+
+// addition in js
+// by declaring another variable which will hold addition result
+var totalPrice = eggPrice + ricePrice
+// printing output
+console.log(totalPrice)
+// addition inside console
+console.log(eggPrice + ricePrice)
+
+// multiplication
+var multiprice = eggPrice * ricePrice
+console.log(multiprice)
+// multiplication inside log
+console.log(eggPrice * ricePrice)
+
+// we can also we can do subtraction and division that way
