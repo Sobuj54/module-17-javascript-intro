d420b67c61dd634ec55a8f550601ab2d363f374f
sobuj <sobujahmed124@gmail.com>
2023-01-27 00:09:02 +0600

    concatenate

---
+++ concatenation.js
@@ -0,0 +1,13 @@
+var firstName = 'sobuj'
+var lastName = 'Ahmed'
+
+// adding or concatenating two strings
+
+var fullName = firstName + lastName
+// but this concatenation does not give white space between two strings
+console.log(fullName)
+
+// new code to give white space between two strings
+fullName = firstName + ' ' + lastName
+// in this output we have white space between two strings because we gave white space between them
+console.log(fullName)
