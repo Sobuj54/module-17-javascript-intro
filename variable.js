beaa158c94d364bc7748d96d3fbd60289e016932
sobuj <sobujahmed124@gmail.com>
2023-01-26 20:18:19 +0600

    variables

--- variable.js
+++ variable.js
@@ -5,3 +5,15 @@ var price = 100
 var weight = 30
 var fair = 28
 var temparature = 22
+
+// strings as variables
+// we need to use double quotation for declaring string varibles
+var address = 'Dhanmondi-15'
+var name = 'sobuj'
+// single quotes can also be used to declare string variables but we can not use both at same string.
+var workplace = 'dhaka'
+
+// boolean
+// boolean varibles dont need quotation because they are keywords
+var exampassed = true
+var isgone = false
